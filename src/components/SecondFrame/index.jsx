ea3427d51d5d7856975b22d70bafa3e279f5f9ec
oliveiradniel <kadadniel@gmail.com>
2024-12-01 14:45:40 -0300

    feat(SecondFrame): add 'CardsContainer' component and description card

--- src/components/SecondFrame/index.jsx
+++ src/components/SecondFrame/index.jsx
@@ -1,4 +1,4 @@
-import { Container, Content } from './styles';
+import { CardsContainer, Container, Content } from './styles';
 
 export default function SecondFrame() {
   return (
@@ -6,6 +6,16 @@ export default function SecondFrame() {
       <div className="frame" />
       <Content>
         <h1 className="title">Serviços</h1>
+        <CardsContainer>
+          <div className="card-description">
+            <p className="card-description-title">Tranforme!</p>
+
+            <p className="description">
+              Veja suas ideias se transformarem em realidade. Serviços de design
+              que comunicam sua marca com excelência!
+            </p>
+          </div>
+        </CardsContainer>
       </Content>
     </Container>
   );
--- src/components/SecondFrame/styles.js
+++ src/components/SecondFrame/styles.js
@@ -21,3 +21,30 @@ export const Content = styled.div`
     font-size: 42px;
   }
 `;
+
+export const CardsContainer = styled.div`
+  display: flex;
+  align-items: center;
+  height: 86%;
+
+  .card-description {
+    background-color: ${({ theme }) => theme.colors.primary.light};
+    border-radius: 4px;
+    height: 300px;
+    padding: 40px 25px;
+    position: relative;
+    transform: translateX(-25%);
+    width: 280px;
+    box-shadow: 14px 14px 40px -22px rgba(0, 0, 0, 0.25);
+
+    .card-description-title {
+      font-size: 26px;
+      font-weight: bold;
+    }
+
+    .description {
+      margin-top: 20px;
+      font-weight: 500;
+    }
+  }
+`;
