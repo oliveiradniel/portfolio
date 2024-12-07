6cd2983497a0e9dd9c716997bafe212c0728a4ca
oliveiradniel <kadadniel@gmail.com>
2024-12-07 12:58:58 -0300

    feat(Modal): create 'Card' component

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,9 +1,36 @@
 import { createPortal } from 'react-dom';
 
-import { Overlay } from './styles';
+import github from '../../assets/icons/github2.svg';
+import styleGuide from '../../assets/icons/style-guide.svg';
+import deploy from '../../assets/icons/deploy.svg';
+import info from '../../assets/icons/info.svg';
+
+import { Container, Overlay } from './styles';
 
 export default function Modal() {
   const container = document.getElementById('modal-root');
 
-  return createPortal(<Overlay>Modal</Overlay>, container);
+  return createPortal(
+    <Overlay>
+      <Container>
+        <div className="card">
+          <img src={github} alt="Github" width={28} />
+          <span>Repositório Github</span>
+        </div>
+        <div className="card">
+          <img src={styleGuide} alt="Guia de Estilo" width={28} />
+          <span>Guia de Estilo</span>
+        </div>
+        <div className="card">
+          <img src={deploy} alt="Deploy" width={28} />
+          <span>Deploy</span>
+        </div>
+        <div className="card">
+          <img src={info} alt="Info" width={28} />
+          <span>Informações Técnicas</span>
+        </div>
+      </Container>
+    </Overlay>,
+    container
+  );
 }
--- src/components/Modal/styles.js
+++ src/components/Modal/styles.js
@@ -6,10 +6,48 @@ export const Overlay = styled.div`
   backdrop-filter: blur(4px);
   display: flex;
   height: 100%;
-  justify-content: center;
   left: 0;
+  justify-content: center;
   position: fixed;
   top: 0;
   width: 100%;
   z-index: 1;
+`;
+
+export const Container = styled.div`
+  display: flex;
+
+  @media (max-width: 1100px) {
+    display: block;
+  }
+
+  .card {
+    align-items: center;
+    background-color: ${({ theme }) => theme.colors.text.dark};
+    display: flex;
+    flex-direction: column;
+    height: 100px;
+    justify-content: center;
+    text-align: center;
+    width: 200px;
+
+    @media (max-width: 1100px) {
+      & + .card {
+        margin-top: 16px;
+      }
+    }
+
+    @media (min-width: 1100px) {
+      & + .card {
+        margin-left: 16px;
+      }
+    }
+
+    span {
+      color: ${({ theme }) => theme.colors.text.lighter};
+      font-size: 14px;
+      font-weight: 500;
+      margin-top: 16px;
+    }
+  }
 `;
